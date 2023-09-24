bd25700d68a98c01c9eb24b01872f3444e4173c1
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-24 19:21:06 +0000

    datatypes

--- 01]_basics/02]_datatypes.js
+++ 01]_basics/02]_datatypes.js
@@ -4,9 +4,22 @@
 /* There are two datatypes in javascript one is PRIMITIVE DATATYPE and second NON-PRIMITIVE DATATYPE(objects in javascript)
 */
 
-let name = "vishal"
-let age =18
-let isloggedIn =false 
+let name = "vishal"  // string
+let age =18  //number
+let isloggedIn =false // boolean
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 4235555334545433n // this n represent bigInt 
+
+
+console.log(typeof "vishal"); // typeof tell type of datatype
+console.log(typeof null);
+
 
 // number => 2 to power 53
 // bigint
@@ -17,7 +30,29 @@ let isloggedIn =false
 // symbol 
 
 
+// NON-PRIMITIVE DATATYPE (Reference)
+
+// Array ,object,Functions 
+
 // object
 
-console.log(typeof "vishal");
-console.log(typeof null);
+// Array 
+const heros =["shaktiman","naagraj","doga"];
+
+console.log(heros);
+
+// Objects
+let myObj ={
+    name:"vishal",
+    age:22,
+}
+
+console.log(myObj);
+// functions
+
+const myFunction = function (){
+    console.log("Hi, vishal");
+}
+
+myFunction();
+
