4ddef099d527f1d008b4212af89e40b62deab295
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-24 17:05:12 +0000

    container config done

---
+++ 01]_basics/02]_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// Datatypes in javascript
+/* There are two datatypes in javascript one is PRIMITIVE DATATYPE and second NON-PRIMITIVE DATATYPE(objects in javascript)
+*/
+
+let name = "vishal"
+let age =18
+let isloggedIn =false 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null =>standalone value
+// undefined 
+// symbol 
+
+
+// object
+
+console.log(typeof "vishal");
+console.log(typeof null);
