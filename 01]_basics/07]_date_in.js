8ed51866b7eed4ccfc2198af40f865a741af8701
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-24 22:00:42 +0000

    Date

---
+++ 01]_basics/07]_date_in.js
@@ -0,0 +1,9 @@
+// Dates
+
+let myDate =new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
