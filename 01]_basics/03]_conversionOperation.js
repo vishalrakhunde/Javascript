64476fd9739a15548643597a22627d832d8b5ebd
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-24 17:29:34 +0000

    Conversion of datatypes

---
+++ 01]_basics/03]_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score ="33"
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score); // This Number convert string into number
+console.log(typeof(valueNumber));
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isloggedIn =1
+let booleanIsLoggedIn = Boolean(isloggedIn) // This boolean convert number into boolean
+console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=>false
+// "" => false
+// "hitesh" => true 
+
+
+let someNumber = 33
+let stringNUmber = String(someNumber); //This String convert number into string
+console.log(stringNUmber);
+
