c0b49d962337242e53fb8d1b502859f3e22d005a
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-24 16:36:01 +0000

    container config done

---
+++ 01]_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1234456
+let accountEmail ="vishal@xyz.com"
+var accountPassword = "12345"
+
+accountCity ="pune"
+let accountState;
+
+// accountId =2 // Can niether be updated nor be re-declared
+
+accountEmail ="vishal@abc.com"
+accountPassword ="6546743"
+accountCity ="mumbai"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
