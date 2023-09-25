634bd1585544622d2cb31b2445de5c5e6631ee22
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-25 08:49:10 +0000

    ConversionOperations

--- 01]_basics/03]_conversionOperation.js
+++ 01]_basics/03]_conversionOperation.js
@@ -43,7 +43,7 @@ let str1 ="hello"
 let str2 ="Vishal"
 console.log(str1+" "+str2);
 
-console.log("1"+2);
+console.log("1"+2); // all convert into string
 console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");
