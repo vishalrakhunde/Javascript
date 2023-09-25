dea012da4414786c3e2c8791bbe76c951070c0f3
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-25 20:49:08 +0000

    Objects part 2

---
+++ 02]_basics/04]_object.js
@@ -0,0 +1,63 @@
+// const tinderUser = new Object() 
+
+const tinderUser ={}
+
+tinderUser.id ="123abc"
+tinderUser.name="vishal"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser ={
+    email:"user@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"vishal",
+            lastname:"rakhunde"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 ={1:"a", 2:"b"}
+const obj2 ={3:"a", 3:"b"}
+const obj3 ={4:"a", 6:"b"}
+
+
+// method one to assign object 
+// const obj4 = Object.assign({},obj1,obj2,obj3) // here this first is empty object {} in which all the source obj will assign
+// if we didn't give that all the object will assign in obj1 
+
+
+
+// method two to assign objects using spread 
+const obj4 ={...obj1,...obj2,...obj3}
+console.log(obj4);
+
+const users = [
+    {
+     id: 1,
+     email: "u@gmail.com"
+    },
+    {
+     id: 2,
+     email: "v@gmail.com"
+    },
+    {
+     id: 3,
+     email: "w@gmail.com"
+    }
+ ];
+ 
+users[1].email // because this is array so use .email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));  // keys in objects => return in arrays
+console.log(Object.values(tinderUser));  // values in objects => return in arrays
+console.log(Object.entries(tinderUser));
+
+// checking if there is value present in object => return in boolean
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
