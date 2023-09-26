61b90ef32acd6ea55b6d464ee5859c89d8e0c669
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-26 13:00:08 +0000

    comaparision

--- 01]_basics/04]_comparision.js
+++ 01]_basics/04]_comparision.js
@@ -4,18 +4,28 @@
 // console.log(2==1);
 // console.log(2!=1);
 
-console.log("2">1);
-console.log("02">1);
+console.log("2">1);  // true
+console.log("02">1);  // true
+ 
+console.log(null >0);  // false
+console.log(null==0);   // false
+console.log(null>=0);    //true
 
-console.log(null >0);
-console.log(null==0);
-console.log(null>=0);
-
-console.log(undefined==0);
-console.log(undefined>0);
-console.log(undefined>=0);
+console.log(undefined==0);  // false
+console.log(undefined>0);    // false
+console.log(undefined>=0);    //false
 
 // === check equal type and equal value
 
-console.log("2" ===2);
+console.log("2" ===2);  //false
 
+console.log(2<12); // true
+console.log(2<"12");     //true  // here Js convert string to number
+console.log(2<"john");   //false  // john cannot be converted to number
+console.log(2>"john");   //false  //
+console.log(2=="john");  //false
+console.log("2"<"12");   //false //JavaScript compares strings by comparing their characters one by one, starting from the beginning. In this case, the first character of "2" is less than the first character of "12", so the result is 
+console.log("2">"12");   //true  // first char of "2" is less than first char of "12"
+console.log("2"=="12");  //false
+
+// When comparing two strings, "2" will be greater than "12", because (alphabetically) 1 is less than 2.
