567b43f680d43aee22476be148099c0e77e4b4fb
Vishal rakhunde <112006115+vishalrakhunde@users.noreply.github.com>
2023-09-27 18:52:56 +0000

    while and do while loop

---
+++ 05]_loops/two.js
@@ -0,0 +1,27 @@
+// While loop
+
+let index =0
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ['flash',"batman","superman"]
+
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr= arr+1
+
+}
+
+// Do while loop
+
+
+// first run onces and then check the condition
+let score =11; // here 11 will print onces
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
